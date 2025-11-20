1002d5ed6603c77ad1e3984f4cdd9098e141a95a
Brian McGrail <brianmcgrail@macbookpro.mynetworksettings.com>
2025-11-20 10:34:59 -0500

    Updated models for Week 12

--- pages/05_Against_the_Spread.py
+++ pages/05_Against_the_Spread.py
@@ -53,6 +53,18 @@ X_train, X_test, y_train, y_test = train_test_split(
 ats_model = LogisticRegression(max_iter=2000, solver="liblinear")
 ats_model.fit(X_train, y_train)
 
+# Team names map/dictionary
+team_name_map = {
+    "KAN": "Chiefs", "CLT": "Colts", "CIN": "Bengals", "NWE": "Patriots",
+    "CHI": "Bears", "PIT": "Steelers", "RAV": "Ravens", "NYJ": "Jets",
+    "OTI": "Titans", "SEA": "Seahawks", "DET": "Lions", "NYG": "Giants",
+    "GNB": "Packers", "MIN": "Vikings", "RAI": "Raiders", "CLE": "Browns",
+    "CRD": "Cardinals", "JAX": "Jaguars", "NOR": "Saints", "ATL": "Falcons",
+    "DAL": "Cowboys", "PHI": "Eagles", "RAM": "Rams", "TAM": "Buccaneers",
+    "SFO": "49ers", "CAR": "Panthers", "HTX": "Texans", "BUF": "Bills",
+}
+
+
 # Prediction Helpers
 def get_team_features(latest_df, season, team, spread, total, home, stat_cols_for_avg):
     subset = latest_df[(latest_df["Season"] == season) & (latest_df["Team"] == team)]
@@ -86,11 +98,15 @@ def run_weekly_ats_predictions(model, df, season, week, matchups, stat_cols_for_
         ats_winner = home_team if pred == 1 else away_team
 
         all_preds.append({
-            "Matchup": f"{away_team} @ {home_team}",
-            "Home Cover Probability": f"{home_prob:.2%}",
-            "Predicted ATS Winner": ats_winner
+            "Matchup": f"{team_name_map.get(away_team, away_team)} @ {team_name_map.get(home_team, home_team)}",
+            "Home Cover Probability": home_prob,
+            "Home Team Spread": spread,
+            "Predicted ATS Winner": team_name_map.get(ats_winner, ats_winner)
         })
-    return pd.DataFrame(all_preds)
+
+    results_df = pd.DataFrame(all_preds)
+    return results_df.sort_values("Home Cover Probability", ascending=False).reset_index(drop=True)
+
 
 # Week 12 Matchups
 season, week = 2025, 12
